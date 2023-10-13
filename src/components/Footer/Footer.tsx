069c10848412cd876b622433f822e656f30bb321
BiBCast <pietrocast03@gmail.com>
2023-10-13 12:02:11 +0200

    feature: hover status

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import { HeadPhones } from "../../assets/HeadPhones";
 import { ChannelIcons } from "../ChannelIcons/ChannelIcons";
 import "./index.css";
 export function Footer() {
+  const [isHover, setIsHover] = useState(false);
   return (
     <footer className="cnSideBar_footer">
-      <div className="footer_user">
+      <div
+        className="footer_user"
+        onMouseOut={() => setIsHover(false)}
+        onMouseOver={() => setIsHover(true)}
+      >
         <div>
           <img src="/ChannelIcon1.webp" alt="" />
         </div>
         <div className="user_status">
           <h3>BiBCast</h3>
-          <p>Online</p>
+          <div className={isHover ? "status status_hover" : "status"}>
+            <p>Online</p>
+            <p className="footer_hover">bibcastbib</p>
+          </div>
         </div>
       </div>
       <ChannelIcons>
