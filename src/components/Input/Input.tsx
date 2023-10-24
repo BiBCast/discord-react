aa7aa912642b7712f54e85cec235aa1f70ae32b2
BiBCast <pietrocast03@gmail.com>
2023-10-24 19:30:25 +0200

    feature: draft chat

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 export function Input() {
   return (
     <div className="input">
-      <input type="seach" />
+      <input type="seach" placeholder="Cerca" />
       <ChannelIcons>
         <Glass />
         <></>
