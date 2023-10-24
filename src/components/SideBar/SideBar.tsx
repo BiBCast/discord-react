aa473700ab845933cc8deb170ee05cdb3d3dbd34
BiBCast <pietrocast03@gmail.com>
2023-10-24 11:29:36 +0200

    fix: align layout

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -3,8 +3,10 @@ import { Icons } from "../Icons/Icons";
 import "./index.css";
 export function SideBar() {
   return (
-    <aside>
-      <Icon />
+    <aside className="Sidebar">
+      <div className="sidebar__icon">
+        <Icon />
+      </div>
       <div className="separator"></div>
       <Icons />
     </aside>
