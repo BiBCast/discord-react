4309816bf6faa47bb0566ac592eb2900595ecd9c
BiBCast <pietrocast03@gmail.com>
2023-10-24 12:35:52 +0200

    feature: date chat separetor

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -10,20 +10,34 @@ export function Chat({ inputs }: { inputs: string[] }) {
   };
 
   useEffect(scrollToBottom, [inputs.length]);
+  const now = new Date();
   return (
     <article className="chat">
       <div className="chat__space"></div>
       {inputs.map((e, index) => (
-        <div key={index} className="chat__input">
-          <Icon />
-          <div>
-            <div className="chat__name_data">
-              <p>Pietro</p>
-              <DateTime />
+        <>
+          <div className="chat__sep_container">
+            <div className="chat_separator"></div>
+            <div className="chat__date">
+              {now.toLocaleString("it-IT", {
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+              })}
             </div>
-            <div className="chat__text">{e}</div>
+            <div className="chat_separator"></div>
           </div>
-        </div>
+          <div key={index} className="chat__input">
+            <Icon />
+            <div>
+              <div className="chat__name_data">
+                <p>Pietro</p>
+                <DateTime />
+              </div>
+              <div className="chat__text">{e}</div>
+            </div>
+          </div>
+        </>
       ))}
       <div ref={bottomEl}></div>
     </article>
